de743bc579d158c823350e1f74bd12515b61fce5
hongci9999 <hongci9999@gmail.com>
2025-06-24 22:05:11 +0900

    작동 확인 완료

--- src/api/posts.ts
+++ src/api/posts.ts
@@ -9,25 +9,19 @@ export interface Post {
 }
 
 export const getPosts = async (ordering: string = '-created_at') => {
-  const res = await axios.get<Post[]>(
-    `http://localhost:8000/api/list-posts/?ordering=${ordering}`
-  );
+  const res = await axios.get<Post[]>(`http://localhost:8000/api/posts/list-posts/?ordering=${ordering}`);
   return res.data;
 };
 
-export const createPost = async (
-  title: string,
-  content: string,
-  accessToken: string
-) => {
+export const createPost = async (title: string, content: string, accessToken: string) => {
   const res = await axios.post(
-    'http://localhost:8000/api/create-post/',
+    'http://localhost:8000/api/posts/create-post/',
     { title, content },
     {
       headers: {
         Authorization: `Bearer ${accessToken}`,
       },
-    }
+    },
   );
   return res.data;
 };
