c1599b687e286bfbac2600769e2b2e440afcee73
rp1028 <lkw20001028@gmail.com>
2025-11-16 23:54:21 +0900

    Update posts.ts

--- src/api/posts.ts
+++ src/api/posts.ts
@@ -151,3 +151,17 @@ export const deletePost = async (postId: number, accessToken: string) => {
   });
   return res.data;
 };
+
+// 게시물 좋아요 토글
+export const togglePostLike = async (postId: number, accessToken: string) => {
+  const res = await axios.post(
+    `${API_BASE}/${postId}/like/`,
+    {},
+    {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    }
+  );
+  return res.data;
+};
