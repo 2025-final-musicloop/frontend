b362e1a864779f4c0376281456a4be959917f87b
rp1028 <lkw20001028@gmail.com>
2025-11-23 02:40:58 +0900

    Update CompletionPage.tsx

--- src/pages/GenreConversion/components/CompletionPage.tsx
+++ src/pages/GenreConversion/components/CompletionPage.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';  // ⬅️ 추가!
 import { ProcessingResult, MusicDetails } from '../../../components/common/ProcessFlow';
 import styles from './CompletionPage.module.css';
 import { useAuth } from '../../../hooks/useAuth';
